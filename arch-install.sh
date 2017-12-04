40bf8c3fa100e05ba206a1d1e6d62f28aa4c2e24
bh1rio <bh1rio@just4fun.cn>
2017-12-04 19:03:28 +0800

    继续修改sh

--- arch-install.sh
+++ arch-install.sh
@@ -4,3 +4,9 @@ parted -s -m /dev/sda mkpart primary ext4 8GB 100%
 mkswap /dev/sda1
 swapon /dev/sda1
 mkfs.ext4 /dev/sda2
+mount /dev/sda2 /mnt
+echo "Server = https://mirrors.ustc.edu.cn/archlinux/$repo/os/$arch" > /etc/pacman.d/mirrorlist
+pacstrap -i /mnt
+genfstab -U -p /mnt >> /mnt/etc/fstab
+arch-chroot /mnt echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+arch-chroot /mnt locale-gen
