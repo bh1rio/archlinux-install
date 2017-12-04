cba0d7f0a04fd5edb7f685ca3b2b69fa5232389b
bh1rio <bh1rio@just4fun.cn>
2017-12-04 22:59:17 +0800

    fix bug

--- arch-install.sh
+++ arch-install.sh
@@ -12,9 +12,9 @@ echo "en_US.UTF-8 UTF-8" > /mnt/etc/locale.gen
 arch-chroot /mnt locale-gen
 echo LANG=en_US.UTF-8 > /mnt/etc/locale.conf
 rm /mnt/etc/localtime
-arch-chroot /mnt ln –s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+arch-chroot /mnt ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 arch-chroot /mnt hwclock --systohc --utc
 echo black > /mnt/etc/hostname
-arch-chroot /mnt mkinitcpio –p linux
+arch-chroot /mnt mkinitcpio -p linux
 arch-chroot /mnt passwd
 arch-chroot /mnt pacman -Sq grub
