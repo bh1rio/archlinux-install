5daebe4fb154a30edae668ae76c4051afd343dfc
bh1rio <bh1rio@just4fun.cn>
2017-12-04 19:17:47 +0800

    修改转义符

--- arch-install.sh
+++ arch-install.sh
@@ -5,7 +5,7 @@ mkswap /dev/sda1
 swapon /dev/sda1
 mkfs.ext4 /dev/sda2
 mount /dev/sda2 /mnt
-echo "Server = https://mirrors.ustc.edu.cn/archlinux/$repo/os/$arch" > /etc/pacman.d/mirrorlist
+echo "Server = https://mirrors.ustc.edu.cn/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
 pacstrap -i /mnt
 genfstab -U -p /mnt >> /mnt/etc/fstab
 arch-chroot /mnt echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
