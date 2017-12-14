dc414aecede9fe3356af5d0843869f146ae2187f
Chi Zhang <bh1rio@just4fun.cn>
2017-12-14 17:06:23 +0800

    Update cinnamon-install.sh

--- cinnamon-install.sh
+++ cinnamon-install.sh
@@ -1,3 +1,7 @@
 pacman -S xorg xorg-drivers xorg-xinit
 pacman -S cinnamon gnome-terminal
 pacman -S noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra
+pacman -S libreoffice-fresh libreoffice-fresh-zh-CN
+pacman -S chromium firefox firefox-i18n-zh-cn
+pacman -S jdk8-openjdk eclipse-jee pycharm intellij-idea-community-edition atom
+pacman -S dotnet-runtime
