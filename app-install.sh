7af8d66205517832641fa15df6d6f3034d4d2394
Chi Zhang <bh1rio@just4fun.cn>
2018-01-12 22:04:27 +0800

    Create app-install.sh

---
+++ app-install.sh
@@ -0,0 +1,6 @@
+pacman -S chromium firefox firefox-i18n-zh-cn thunderbird thunderbird-i18n-zh-cn
+pacman -S filezilla
+pacman -S noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra
+pacman -S libreoffice-fresh libreoffice-fresh-zh-CN
+pacman -S jdk8-openjdk eclipse-jee pycharm intellij-idea-community-edition atom
+pacman -S dotnet-runtime
