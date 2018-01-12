79e5fbf48f7bbc82f99d7de24dec48f14bd2eded
Chi Zhang <bh1rio@just4fun.cn>
2018-01-12 22:13:38 +0800

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -1,14 +1,12 @@
 parted -s -m /dev/sda mktable msdos
-parted -s -m /dev/sda mkpart primary linux-swap 0% 8GB
-parted -s -m /dev/sda mkpart primary ext4 8GB 100%
-mkswap /dev/sda1
-swapon /dev/sda1
-mkfs.ext4 /dev/sda2
-mount /dev/sda2 /mnt
+parted -s -m /dev/sda mkpart primary ext4 0% 100%
+mkfs.ext4 /dev/sda1
+mount /dev/sda1 /mnt
 echo "Server = https://mirrors.ustc.edu.cn/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
-pacstrap /mnt
+pacstrap /mnt base base-devel
 genfstab -U -p /mnt >> /mnt/etc/fstab
 echo "en_US.UTF-8 UTF-8" > /mnt/etc/locale.gen
+echo "zh_CN.UTF-8 UTF-8" >> /mnt/etc/locale.gen
 arch-chroot /mnt locale-gen
 echo LANG=en_US.UTF-8 > /mnt/etc/locale.conf
 rm /mnt/etc/localtime
@@ -17,7 +15,7 @@ arch-chroot /mnt hwclock --systohc --utc
 echo black > /mnt/etc/hostname
 arch-chroot /mnt mkinitcpio -p linux
 #arch-chroot /mnt passwd
-arch-chroot /mnt pacman -S --noconfirm grub dialog wpa_supplicant
+arch-chroot /mnt pacman -S --noconfirm grub dialog wpa_supplicant intel-ucode wget git
 arch-chroot /mnt grub-install --target=i386-pc /dev/sda
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 umount -R /mnt
