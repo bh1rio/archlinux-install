cbd1ed7f3a702ce1af5025a1e08e9314e84fc522
Chi Zhang <bh1rio@just4fun.cn>
2018-02-10 19:51:58 +0800

    Update cinnamon-install.sh

--- cinnamon-install.sh
+++ cinnamon-install.sh
@@ -1,4 +1,4 @@
-pacman -S --noconfirm xorg xorg-xinit
+pacman -S --noconfirm xorg xorg-xinit xf86-video-intel xf86-input-synaptics
 pacman -S --noconfirm cinnamon gnome-terminal
 wget https://github.com/bh1rio/archlinux-install/raw/master/xinitrc
 mv xinitrc .xinitrc
