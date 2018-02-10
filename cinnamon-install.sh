295429ba3d6c10cb667cfe0bec676a8c732c0a4c
Chi Zhang <bh1rio@just4fun.cn>
2018-02-10 20:35:25 +0800

    Update cinnamon-install.sh

--- cinnamon-install.sh
+++ cinnamon-install.sh
@@ -1,5 +1,6 @@
 pacman -S --noconfirm xorg xorg-xinit xf86-video-intel xf86-input-synaptics
-pacman -S --noconfirm cinnamon gnome-terminal
+pacman -S --noconfirm cinnamon
+pacman -S --noconfirm gnome-terminal
 pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra
 wget https://github.com/bh1rio/archlinux-install/raw/master/xinitrc
 mv xinitrc .xinitrc
