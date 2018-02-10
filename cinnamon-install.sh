053d7e6483982f7baa9338828d6543a89c3a28ca
Chi Zhang <bh1rio@just4fun.cn>
2018-02-10 23:58:19 +0800

    Update cinnamon-install.sh

--- cinnamon-install.sh
+++ cinnamon-install.sh
@@ -1,4 +1,7 @@
 pacman -S --noconfirm xorg xorg-xinit xf86-video-intel xf86-input-synaptics
 pacman -S --noconfirm cinnamon
+pacman -S --noconfirm gnome-terminal
 wget https://github.com/bh1rio/archlinux-install/raw/master/xinitrc
 mv xinitrc .xinitrc
+wget https://github.com/bh1rio/archlinux-install/raw/master/app-install.sh
+chmod +x app-install.sh
