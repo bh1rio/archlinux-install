78b014396f6911cec41472a44506c7a45e50c2fd
Chi Zhang <bh1rio@just4fun.cn>
2018-02-10 23:59:47 +0800

    Update xfce4-install.sh

--- xfce4-install.sh
+++ xfce4-install.sh
@@ -3,3 +3,5 @@ pacman -S xfce4 xfce4-goodies xfce4-terminal lightdm-gtk-greeter lightdm-gtk-gre
 pacman -S galculator midori parole ristretto
 wget https://github.com/bh1rio/archlinux-install/raw/master/xinitrc-xfce4
 mv xinitrc-xfce4 .xinitrc
+wget https://github.com/bh1rio/archlinux-install/raw/master/app-install.sh
+chmod +x app-install.sh
