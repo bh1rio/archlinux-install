1443ff6527a8d8bac5ec5bf3d74b29b6ac9c46d3
Chi Zhang <bh1rio@just4fun.cn>
2018-02-10 21:05:20 +0800

    Update app-install.sh

--- app-install.sh
+++ app-install.sh
@@ -1,5 +1,7 @@
+pacman -S --noconfirm gnome-terminal
+pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra
 pacman -S --noconfirm chromium firefox firefox-i18n-zh-cn thunderbird thunderbird-i18n-zh-cn
 pacman -S --noconfirm filezilla
 pacman -S --noconfirm libreoffice-fresh libreoffice-fresh-zh-CN
 pacman -S --noconfirm jdk8-openjdk eclipse-jee pycharm intellij-idea-community-edition atom
-pacman -S --noconfirm dotnet-runtime
+pacman -S --noconfirm dotnet-sdk
