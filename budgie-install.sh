51053af22e6c8b8f9621e39c5d5edc4f76ae8130
Chi Zhang <bh1rio@just4fun.cn>
2019-05-28 10:05:09 +0800

    Update budgie-install.sh

--- budgie-install.sh
+++ budgie-install.sh
@@ -1,5 +1,5 @@
 pacman -S --noconfirm xorg xorg-xinit xf86-video-intel xf86-input-synaptics
-pacman -S --noconfirm budgie-desktop gnome gnome-extra  lightdm-gtk-greeter lightdm-gtk-greeter-settings
+pacman -S --noconfirm budgie-desktop budgie-extras  lightdm-gtk-greeter lightdm-gtk-greeter-settings
 pacman -S --noconfirm galculator midori parole ristretto
 wget https://github.com/bh1rio/archlinux-install/raw/master/xinitrc-xfce4
 mv xinitrc-budgie .xinitrc
