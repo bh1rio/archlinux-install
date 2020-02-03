60fe43bd797b3ab2889b9d1bdae21fe377e52bdf
Chi Zhang <bh1rio@just4fun.cn>
2020-02-04 01:23:20 +0800

    添加xterm防止无法启动

--- budgie-install.sh
+++ budgie-install.sh
@@ -1,4 +1,4 @@
-pacman -S --noconfirm xorg xorg-xinit xf86-video-intel xf86-input-synaptics
+pacman -S --noconfirm xorg xorg-xinit xf86-video-intel xf86-input-synaptics xterm
 pacman -S --noconfirm budgie-desktop budgie-extras  lightdm-gtk-greeter lightdm-gtk-greeter-settings
 pacman -S --noconfirm galculator midori parole ristretto
 wget https://github.com/bh1rio/archlinux-install/raw/master/xinitrc-budgite
