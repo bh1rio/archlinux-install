6541a64ccd4d98a7a28ea983cef01d9f6810f1b3
Chi Zhang <bh1rio@just4fun.cn>
2020-02-04 00:58:39 +0800

    修正libreoffice中文包的名字

--- app-install.sh
+++ app-install.sh
@@ -2,7 +2,7 @@ pacman -S --noconfirm fcitx-im fcitx-configtool fcitx-googlepinyin
 pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra
 pacman -S --noconfirm chromium firefox firefox-i18n-zh-cn thunderbird thunderbird-i18n-zh-cn
 pacman -S --noconfirm filezilla putty
-pacman -S --noconfirm libreoffice-fresh libreoffice-fresh-zh-CN
+pacman -S --noconfirm libreoffice-fresh libreoffice-fresh-zh-cn
 pacman -S --noconfirm jdk8-openjdk eclipse-jee atom
 pacman -S --noconfirm dotnet-sdk
 pacman -S --noconfirm go sudo
