68bcc7424b0e0e801d6aea3fe1f3c021ee9165c2
Chi Zhang <bh1rio@just4fun.cn>
2022-01-01 15:26:47 +0800

    Update and rename arch-install.sh to arch-bios-install.sh

--- arch-bios-install.sh
+++ arch-bios-install.sh
@@ -2,20 +2,20 @@ parted -s -m /dev/sda mktable msdos
 parted -s -m /dev/sda mkpart primary ext4 0% 100%
 mkfs.ext4 /dev/sda1
 mount /dev/sda1 /mnt
-echo "Server = https://mirrors.ustc.edu.cn/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
-pacstrap /mnt base linux linux-firmware netctl dhcpcd vi
-genfstab -U -p /mnt >> /mnt/etc/fstab
+echo "Server = https://mirrors.tuna.tsinghua.edu.cn/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
+pacstrap /mnt base linux linux-firmware vi sudo
+genfstab -U /mnt >> /mnt/etc/fstab
 echo "en_US.UTF-8 UTF-8" > /mnt/etc/locale.gen
-echo "zh_CN.UTF-8 UTF-8" >> /mnt/etc/locale.gen
+# echo "zh_CN.UTF-8 UTF-8" >> /mnt/etc/locale.gen
 arch-chroot /mnt locale-gen
 echo LANG=en_US.UTF-8 > /mnt/etc/locale.conf
 rm /mnt/etc/localtime
 arch-chroot /mnt ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 arch-chroot /mnt hwclock --systohc --utc
-echo deepblack > /mnt/etc/hostname
+echo arch > /mnt/etc/hostname
 arch-chroot /mnt mkinitcpio -p linux
-#arch-chroot /mnt passwd
-arch-chroot /mnt pacman -S --noconfirm grub dialog wpa_supplicant intel-ucode wget git
+# arch-chroot /mnt passwd
+arch-chroot /mnt pacman -S --noconfirm grub intel-ucode
 arch-chroot /mnt grub-install --target=i386-pc /dev/sda
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 umount -R /mnt
